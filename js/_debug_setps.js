0206b415f279f752feaa19d3dcc6d5b8fc71d114
ImonShah <Imonshah763@gmail.com>
2023-03-13 17:53:23 +0600

    first commit

---
+++ js/_debug_setps.js
@@ -0,0 +1,8 @@
+/* 
+Debug Steps
+1. Go to the live website. And try to reproduce the error.
+2. Check the console of the live website to seea ny error/exception
+3. Read exception thorowghly. in detail
+4. If the error is new to you: Copy the text of the error and search online
+5. 
+*/
