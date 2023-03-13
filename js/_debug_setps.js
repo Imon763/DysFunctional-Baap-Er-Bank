de9305e1e0e7f8a780ba3a337abe11b0e268967d
ImonShah <Imonshah763@gmail.com>
2023-03-14 00:07:23 +0600

    wihtdraw error solved

--- js/_debug_setps.js
+++ js/_debug_setps.js
@@ -4,5 +4,14 @@ Debug Steps
 2. Check the console of the live website to seea ny error/exception
 3. Read exception thorowghly. in detail
 4. If the error is new to you: Copy the text of the error and search online
-5. 
+5. if a line number is showing, click on that line to see the code
+6. Add breakpoint. re run the code (refresh the website). and see whether you are getting the expected values
+   6.1: if you are not getting the expected values, check which values you are not getting. if needed a break point in the previous line\
+   6.2: If needed, step into (F11) to a function
+7. 
 */
+
+
+/* 
+innerText --> value
+ */
